d0ec2277782f854cb69959d22fae1cd861029a31
Zaid <zaidwaheed68@gmail.com>
2023-07-22 15:18:18 +0500

    22 July

--- addcart.php
+++ addcart.php
@@ -14,14 +14,64 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // For example, you can access the form data like this:
         // $itemName = "Prawn And Haddock Fish Pie";
         // $price = 75;
+        
     } elseif (isset($_POST["insert-into-cart"]) && $_POST["insert-into-cart"] === "ss") {
-        // Form 2 (Shrimp Scampi) was submitted
-        // Handle the data from Form 2 here
+        $name = "Shrimp Scampi";
+        // Handle the data from Form 1 here
+        $query = "INSERT INTO additems (id, name, price) VALUES (NULL, '$name', 50)";
+        mysqli_query($con, $query);
+        header("location: index.html");
+    }
+    
+    elseif (isset($_POST["insert-into-cart"]) && $_POST["insert-into-cart"] === "mspc") {
+        $name = "Mediterranean Seafood With Polenta Cubes";
+        // Handle the data from Form 1 here
+        $query = "INSERT INTO additems (id, name, price) VALUES (NULL, '$name', 120)";
+        mysqli_query($con, $query);
+        header("location: index.html");
+    }
 
-        // For example, you can access the form data like this:
-        // $itemName = "Shrimp Scampi";
-        // $price = 50;
-    } else {
+    elseif (isset($_POST["insert-into-cart"]) && $_POST["insert-into-cart"] === "gltgb") {
+        $name = "Grilled Lobster Tails with Garlic Butter";
+        // Handle the data from Form 1 here
+        $query = "INSERT INTO additems (id, name, price) VALUES (NULL, '$name', 115)";
+        mysqli_query($con, $query);
+        header("location: index.html");
+    }
+    
+    elseif (isset($_POST["insert-into-cart"]) && $_POST["insert-into-cart"] === "coscr") {
+        $name = "Chipotle Orange Shrimp With Cilantro Rice";
+        // Handle the data from Form 1 here
+        $query = "INSERT INTO additems (id, name, price) VALUES (NULL, '$name', 80)";
+        mysqli_query($con, $query);
+        header("location: index.html");
+    }
+
+    elseif (isset($_POST["insert-into-cart"]) && $_POST["insert-into-cart"] === "sfs") {
+        $name = "Spanish Fish Stew";
+        // Handle the data from Form 1 here
+        $query = "INSERT INTO additems (id, name, price) VALUES (NULL, '$name', 95)";
+        mysqli_query($con, $query);
+        header("location: index.html");
+    }
+
+    elseif (isset($_POST["insert-into-cart"]) && $_POST["insert-into-cart"] === "gfs") {
+        $name = "Grilled Fish Steak";
+        // Handle the data from Form 1 here
+        $query = "INSERT INTO additems (id, name, price) VALUES (NULL, '$name', 100)";
+        mysqli_query($con, $query);
+        header("location: index.html");
+    }
+
+    elseif (isset($_POST["insert-into-cart"]) && $_POST["insert-into-cart"] === "fcns") {
+        $name = "Fish Curry With Nigella Seeds";
+        // Handle the data from Form 1 here
+        $query = "INSERT INTO additems (id, name, price) VALUES (NULL, '$name', 60)";
+        mysqli_query($con, $query);
+        header("location: index.html");
+    }
+
+    else {
         // Invalid form submission
         // You can handle the error or redirect back to the page with an error message
         header("Location: index.php?error=Invalid%20form%20submission");
@@ -30,9 +80,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // Perform the necessary actions with the form data, such as adding to cart, storing in the database, etc.
     // ...
-
+    
     // After processing the form data, you can redirect the user to a confirmation page or the same page with a success message
     // header("Location: confirmation.php");
+    
     exit;
 }
 ?>
